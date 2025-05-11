3cab54fbd113e33603b782535de2af9ee3d65806
linabambague04 <linabambague444@gmail.com>
2025-05-10 22:46:25 -0500

    Commit 1 - repaired

--- database/migrations/2025_05_11_020539_create_event_user_table.php
+++ database/migrations/2025_05_11_020539_create_event_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('attendees', function (Blueprint $table) {
+        Schema::create('event_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('attendees');
+        Schema::dropIfExists('event_user');
     }
 };
