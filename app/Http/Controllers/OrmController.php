0b4cb18deb2a362ae2d5176497dc3d69dd9225ac
Alvaro Yunda <yundaalvaro081@gmail.com>
2025-05-13 12:28:30 -0500

    commit9

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -14,8 +14,20 @@ class OrmController extends Controller
 {
     public function consultas(){
 
-        //$user = User::find(2);// los servicios que ofrece el usuario con id 2 proveedor lina
-        //return $user->services;
+        /*$user = User::find(2);// los servicios que ofrece el usuario con id 2 proveedor lina
+        return $user->services;*/
+
+       /* $event=Event::find(2);// las personas que asisten al evento co id 2
+        return $event->users;*/
+
+        /*$event = event::find(2);
+        return $event->resources;// los recursos que tiene el evento con id 2*/
+
+        /*$event = event::find(1); 
+        return $event->feedbacks;// los feedbacks que tiene el evento con id 1*/
+
+        /*$event = event::find(1);
+        return $event;// los eventos que tiene el evento con id 1*/
 
         //$users = User::all(); // todos los usuarios
         //return $users;
@@ -30,14 +42,15 @@ class OrmController extends Controller
     ])->get();*/
 
        return User::with([
-        'events',
-        'events.resources',
-        'events.notifications',
-        'events.feedbacks',
-        'feedbacks',
-        'services'
+        'events',// todos los eventos que tiene el usuario
+        'events.resources',// todos los recursos que tiene el evento
+        'events.notifications',// todos los eventos que tiene el usuario
+        'events.feedbacks',// todos los feedbacks que tiene el evento
+        'feedbacks',// todos los feedbacks que tiene el usuario
+        'services'// todos los servicios que tiene el usuario
 
-    ])->get();    
+    ])->get();
+        
    /*
     $user = User::with('events')->get();// todos los eventos de los usuarios
         return $user;*/
