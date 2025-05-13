5da4e5f3280e761d1eaada067f8ab8089c53b270
Alvaro Yunda <yundaalvaro081@gmail.com>
2025-05-12 21:30:04 -0500

    commit7

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Feedback;
+use App\Models\Notification;
+use App\Models\Resource;
+use App\Models\Service;
+use App\Models\User;
 
 class OrmController extends Controller
 {
-    //
+    public function consultas(){
+
+       // $user = User::find(2);
+        //return $user->services;
+        $print = "hola";
+        return $print;
+    }
 }
